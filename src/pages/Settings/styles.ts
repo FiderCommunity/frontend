ab8f306b8305949d2c74d30e25fbcb12cb8fd6a4
Bernardo Henrique <bernardohrl.unb@gmail.com>
2021-10-12 17:59:27 -0300

    Styles Projects List

--- src/pages/Settings/styles.ts
+++ src/pages/Settings/styles.ts
@@ -2,22 +2,42 @@ import styled from 'styled-components';
 
 
 export const Container = styled.div`
-  // height: 100vh;
   margin: 2.5% 5%;
-  display: flex;
-  // align-items: stretch;
 `;
 
-export const Input = styled.input`
-  width: 300px;
-  font-size: 16px;
-  padding: 10px;
-  background-color: #e5e5e5;
-  border: #cfcfcf 0.1px solid;
-  // float: right;
-  margin: 10px 20px;
-`
 
 export const InputList = styled.div`
   display: grid;
+  max-width: 450px;
 `
+
+
+export const ProjectsList = styled.div`
+  margin-bottom: 50px;
+`
+
+export const Card = styled.li`
+  button {
+    border: none;
+    background-color: #e5e5e5;
+  }
+
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  
+  display: inline-flex;
+  list-style: none;
+
+  padding: 15px 15px;
+  margin-bottom: 3%;
+  margin-right: 3%
+
+`;
+
+export const Name = styled.div`
+    text-align: center;
+    color: #312e38;
+    text-decoration: none;
+    margin-right: 15px
+`;
+
+export const CardContainer = styled.ul``
